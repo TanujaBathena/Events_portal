d80ae16c3b87822d95f21608def1e8aa216a368c
noel_vincent <noelvincent555@gmail.com>
2021-07-16 18:22:12 +0530

    noel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,3 +10,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+// this is a;
